ecd2986b0d3b834852c6be017fcf29160603ebcb
seyithan <seyithanalkan@yahoo.com>
2022-12-07 13:24:56 +0300

    added macos script

--- mac_automation.sh
+++ mac_automation.sh
@@ -2,15 +2,14 @@
 source secret.sh
 sed -i '' "s/line2/export AWS_ACCESS_KEY_ID=$my_aws_key/" ec2-secret.sh
 sed -i '' "s/line3/export AWS_SECRET_ACCESS_KEY=$my_secret_key/" ec2-secret.sh
-brew install awscli -y
+brew install awscli 
 source ec2-secret.sh
 aws configure set AWS_ACCESS_KEY_ID $my_aws_key
 aws configure set AWS_SECRET_ACCESS_KEY $my_secret_key
 aws configure set region "eu-central-1" 
 aws configure set output "json" 
 aws configure list
-brew tap hashicorp/tap
-brew install hashicorp/tap/terraform
+brew install terraform
 terraform -v
 terraform init
 terraform plan
